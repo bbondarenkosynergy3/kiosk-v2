a42bfc86c309f43723e4811666f2d2bd70abdf95
bbondarenkosynergy3 <bbondarenko@synergy3.com>
2025-10-22 19:04:38 +0000

    modified:   app/src/main/java/net/synergy360/kiosk/MyFirebaseService.kt

--- app/src/main/java/net/synergy360/kiosk/MyFirebaseService.kt
+++ app/src/main/java/net/synergy360/kiosk/MyFirebaseService.kt
@@ -33,20 +33,21 @@ class MyFirebaseService : FirebaseMessagingService() {
     }
 
     override fun onNewToken(token: String) {
-        Log.d("FCM", "New token: $token")
+    Log.d("FCM", "New token: $token")
 
-        val db = FirebaseFirestore.getInstance()
-        val deviceInfo = hashMapOf(
-            "token" to token,
-            "model" to Build.MODEL,
-            "brand" to Build.BRAND,
-            "sdk" to Build.VERSION.SDK_INT,
-            "timestamp" to System.currentTimeMillis()
-        )
+    val db = FirebaseFirestore.getInstance()
+    val deviceInfo = hashMapOf(
+        "token" to token,
+        "model" to Build.MODEL,
+        "brand" to Build.BRAND,
+        "sdk" to Build.VERSION.SDK_INT,
+        "timestamp" to System.currentTimeMillis()
+    )
 
-        db.collection("devices")
-            .set(deviceInfo)
-            .addOnSuccessListener { Log.d("Firestore", "Device registered successfully!") }
-            .addOnFailureListener { e -> Log.w("Firestore", "Error adding device", e) }
-    }
+    db.collection("devices")
+        .document(Build.MODEL) // 👈 фиксированный ID устройства
+        .set(deviceInfo)
+        .addOnSuccessListener { Log.d("Firestore", "✅ Device registered successfully!") }
+        .addOnFailureListener { e -> Log.w("Firestore", "❌ Error adding device", e) }
+}
 }
