aed53d8eb3767777a0c61dded0a42ab7b452c9ce
bbondarenkosynergy3 <bbondarenko@synergy3.com>
2025-10-22 18:56:23 +0000

    modified:   app/src/main/java/net/synergy360/kiosk/MainActivity.kt

--- app/src/main/java/net/synergy360/kiosk/MainActivity.kt
+++ app/src/main/java/net/synergy360/kiosk/MainActivity.kt
@@ -13,8 +13,7 @@ import android.webkit.WebViewClient
 import android.widget.FrameLayout
 import android.widget.TextView
 import java.util.*
-import com.google.firebase.firestore.firestore
-import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
 import android.os.Build
 
